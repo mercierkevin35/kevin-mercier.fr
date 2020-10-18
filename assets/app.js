d12e278e9f8f231bae4f7a2963261c332e812986
Kévin Mercier <mercier.kevin35@gmail.com>
2020-10-18 04:35:42 +0200

    fix modal cookies bug

--- assets/app.js
+++ assets/app.js
@@ -14,13 +14,23 @@ import CookieManager from './CookieManager.js';
 // import $ from 'jquery';
 
 
+function insertAnalytics(){
+    var head = document.querySelector("head");
+    head.innerHTML = "<script async src=\"https://www.googletagmanager.com/gtag/js?id=UA-180568389-1\"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-180568389-1');</script>" + head.innerHTML;
+}
+
 var cookieManager = new CookieManager();
+
 if(!cookieManager.getCookies().acceptCookies){
     var modal = document.getElementById("cookieModal");
     var modalBtn = document.getElementById("btn-modal");
     modalBtn.addEventListener('click', () => {
         cookieManager.setCookie("acceptCookies", 1);
         modal.style.display = "none";
+        insertAnalytics();
     });
     modal.style.display = "block";
+}
+else{
+    insertAnalytics();
 }
